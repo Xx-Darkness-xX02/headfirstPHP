de7b9653d3b6e14727e6918ba40ae29db4e16ffc
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-09-09 16:37:00 +0200

    x

--- chapter1/abduction form/report.php
+++ chapter1/abduction form/report.php
@@ -1,15 +1,29 @@
 <?php
+$name = $_POST['firstname'] . '' . ['lastname'];
+$how_many = $_POST['howmany'];
 $when_it_happend = $_POST['whenithappened'];
 $how_long = $_POST['howlong'];
 $alien_description = $_POST['aliendescription'];
 $fang_spotted = $_POST['fangspotted'];
 $email = $_POST['email'];
+$what_they_did = $_POST['whattheydid'];
+$other = $_POST['other'];
 
 echo 'thanks for submitting the form<br />';
 echo 'you were abducted ' . $when_it_happend;
 echo ' and you were gone for ' . $how_long . '<br />';
+echo 'Number of aliens ' . $how_many . '<br />';
 echo 'Describe them: ' . $alien_description . '<br />';
+echo 'The aliens did this: ' . $what_they_did . '<br />';
 echo 'Was Fang there? ' . $fang_spotted . '<br />';
+echo 'other comments: ' . $other . '<br />';
 echo 'your email addres is ' . $email;
+
+$msg = "$name was abducted $when_it_happend and was gone for $how_long. \n" .
+    "Number of aliens: $how_many\n" .
+    "alien description $alien_description\n" .
+    "What they did $what_they_did\n" .
+    "Fang spotted $fang_spotted\n" .
+    "other comments $other";
 
 ?>

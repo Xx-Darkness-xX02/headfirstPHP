5e93a56b6e5c686780decae19ebcfe76b34eae3e
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-09-16 16:48:35 +0200

    x

--- chapter 5/addscore.php
+++ chapter 5/addscore.php
@@ -10,31 +10,38 @@
 <h2>Guitar Wars - Add Your High Score</h2>
 
 <?php
+require_once ('connectvars.php');
+require_once ('appvars.php');
+
 if (isset($_POST['submit'])) {
     // Grab the score data from the POST
     $name = $_POST['name'];
     $score = $_POST['score'];
     $screenshot = $_FILES['screenshot']['name'];
 
-    if (!empty($name) && !empty($score)) {
+    if (!empty($name) && !empty($score) && !empty($screenshot)) {
+        $target = GW_UPLOADPATH . $screenshot;
         // Connect to the database
-        $dbc = mysqli_connect('localhost', 'root', '', 'gwdb');
+        if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {
+            $dbc = mysqli_connect('localhost', 'root', '', 'gwdb');
 
-        // Write the data to the database
-        $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score', '$screenshot')";
-        mysqli_query($dbc, $query);
+            // Write the data to the database
+            $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score', '$screenshot')";
+            mysqli_query($dbc, $query);
 
-        // Confirm success with the user
-        echo '<p>Thanks for adding your new high score!</p>';
-        echo '<p><strong>Name:</strong> ' . $name . '<br />';
-        echo '<strong>Score:</strong> ' . $score . '</p>';
-        echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
+            // Confirm success with the user
+            echo '<p>Thanks for adding your new high score!</p>';
+            echo '<p><strong>Name:</strong> ' . $name . '<br />';
+            echo '<strong>Score:</strong> ' . $score . '</p>';
+            echo '<img src="' . GW_UPLOADPATH . $screenshot . '" alt="Score Image" /></p>';
+            echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
 
-        // Clear the score data to clear the form
-        $name = "";
-        $score = "";
+            // Clear the score data to clear the form
+            $name = "";
+            $score = "";
 
-        mysqli_close($dbc);
+            mysqli_close($dbc);
+        }
     } else {
         echo '<p class="error">Please enter all of the information to add your high score.</p>';
     }
@@ -42,12 +49,13 @@ if (isset($_POST['submit'])) {
 ?>
 
 <hr/>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="hidden" name="MAX_FILE_SIZE" value="32768" />
     <label for="name">Name:</label>
     <input type="text" id="name" name="name" value="<?php if (!empty($name)) echo $name; ?>"/><br/>
     <label for="score">Score:</label>
     <input type="text" id="score" name="score" value="<?php if (!empty($score)) echo $score; ?>"/>
-    <input type="file" id="screenshot" name="screenshot" />
+    <input type="file" id="screenshot" name="screenshot"/>
     <hr/>
     <input type="submit" value="Add" name="submit"/>
 </form>
--- chapter 5/index.php
+++ chapter 5/index.php
@@ -12,6 +12,9 @@
 <hr />
 
 <?php
+require_once ('connectvars.php');
+require_once ('appvars.php');
+
 // Connect to the database
 $dbc = mysqli_connect('localhost', 'root', '', 'gwdb');
 
@@ -22,20 +25,26 @@ $data = mysqli_query($dbc, $query);
 // Loop through the array of score data, formatting it as HTML
 echo '<table>';
 while ($row = mysqli_fetch_array($data)) {
+    $i = 0;
+    if($i == 0){
+        echo '<tr><td colspan="2" class="topscoreheader">Top Score:' . $row['score'] .  '</td></tr>';
+    }
+
     // Display the score data
     echo '<tr><td class="scoreinfo">';
     echo '<span class="score">' . $row['score'] . '</span><br />';
     echo '<strong>Name:</strong> ' . $row['name'] . '<br />';
     echo '<strong>Date:</strong> ' . $row['date'] . '</td></tr>';
-    if(is_file($row['screenshot']) && filesize($row['screenshot']) > 0  ){
-        echo '<td><img src="'. $row['screenshot'] .'" alt="score image" /></td></tr>';
+    if(is_file(GW_UPLOADPATH . $row['screenshot']) && filesize(GW_UPLOADPATH . $row['screenshot']) > 0  ){
+        echo '<td><img src="' . GW_UPLOADPATH .  $row['screenshot'] .'" alt="score image" /></td></tr>';
     }
+
     else{
         echo '<td><img src="images/unverified.gif" alt="unverified score" /></td></tr>';
     }
+    $i++;
 }
 echo '</table>';
-
 mysqli_close($dbc);
 ?>
 

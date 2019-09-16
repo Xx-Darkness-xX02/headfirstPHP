bdaed1f95cc6041cd72d3d0f1fab7c9036006727
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-09-16 12:12:42 +0200

    x

--- chapter 4/removeemail.php
+++ chapter 4/removeemail.php
@@ -1,11 +1,29 @@
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"
+
 <?php
 $dbc = mysqli_connect('localhost', 'root', '', 'elvis_store');
 
-$email = $_POST['email'];
+if(isset($_POST['submit'])) {
+    foreach ($_POST['todelete'] as $delete_id) {
+        $query = "DELETE FROM email_list WHERE id=$delete_id";
+        mysqli_query($dbc, $query)
+            or die ('error querying database');
+    }
+        echo 'customer(s) removed <br />';
+}
 
-$query = "DELETE FROM email_list WHERE email = '$email'";
-mysqli_query($dbc, $query);
-
-echo 'your email:' . $email . 'has been removed';
+$query = "SELECT * FROM email_list";
+$result = mysqli_query($dbc, $query);
+while ($row = mysqli_fetch_array($result)){
+    echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]" />';
+    echo $row['first_name'];
+    echo '' . $row['last_name'];
+    echo '' . $row['email'];
+    echo '<br />';
+}
 
 mysqli_close($dbc);
+?>
+
+<input type="submit" name="submit" value="remove" />
+</form>
